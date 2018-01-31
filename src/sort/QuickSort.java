0d45ca50449241c16309313f9f8bc370f9fb0bd9
ANTRED <zxcmy1988@163.com>
2018-02-01 00:11:45 +0800

    快速排序练习

---
+++ src/sort/QuickSort.java
@@ -0,0 +1,23 @@
+package sort;
+/**
+ * 快速排序实现代码
+ * @author XuLongFei
+ * @date 2018年1月31日
+ */
+public class QuickSort {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	/**
+	 * 将数组的某一段元素进行划分，小的在左边，大的在右边
+	 * @param array 要排序的数组
+	 * @param start 
+	 * @param end
+	 */
+	public void sort(Integer []array, int start, int end) {
+		
+	}
+
+}
